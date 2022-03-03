80657f1448bdff3ad001253ca990d6b3e83cd859
sinct1229 <sinct1229@gmail.com>
2022-03-03 22:25:03 +0700

    add vendor testcase

--- src/test/java/vendor/vendor.java
+++ src/test/java/vendor/vendor.java
@@ -5,20 +5,15 @@ import org.testng.annotations.Test;
 
 import commons.GlobalContants;
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class vendor {
 	Response response;
 	String jsonString;
-//	String poNumber = "SINTEST";
-//	String vendorId = "539a2980-6ca7-45a0-84b6-622c4861d2fe";
-//	double date = 1648021921044.0;
-//	double location = 14874116142.0;
-	String createVendor = "1Sin";
+	String createVendor = "6Sin";
 	String updateVendor = "new_Sin";
-	String vendorId = "";
+	String vendorID = "";
 	
 	@Test
 	public void Test_01_Create_Vendor() {
@@ -27,10 +22,9 @@ public class vendor {
 		request.header("Authorization", "Bearer " + GlobalContants.TOKEN).header("x-tenant-id", "melinda").header("Content-Type", "application/json");
 		response = request.body("{\"name\":\"" + createVendor+"\"}").post("/api/integration-svc/vendors");
 		Assert.assertEquals(response.getStatusCode(), 200);
-		String jsonString = response.asString();
-		vendorId = JsonPath.from(jsonString).get("id");
-		System.out.println(vendorId);
-	}
+		Assert.assertEquals(response.jsonPath().get("data.task"), "create");
+		vendorID = response.jsonPath().get("data.data.id");
+		}
 	
 	
 	@Test
@@ -38,18 +32,30 @@ public class vendor {
 		RestAssured.baseURI = GlobalContants.BASE_URL;
 		RequestSpecification request = RestAssured.given();
 		request.header("Authorization", "Bearer " + GlobalContants.TOKEN).header("x-tenant-id", "melinda").header("Content-Type", "application/json");
-		response = request.body("{\"name\":\"" + updateVendor+"\"}").put("/api/integration-svc/vendors");
+		response = request.body("{\"name\":\"" + updateVendor+"\"}").put("/api/integration-svc/vendors/"+ vendorID);
 		Assert.assertEquals(response.getStatusCode(), 200);
+		Assert.assertEquals(response.jsonPath().get("data.task"), "update");
 	}
 	
-
 	@Test
-	public void Test_03_Delete_Vendor() {
+	public void Test_03_Get_Info_Of_A_Vendor() {
 		RestAssured.baseURI = GlobalContants.BASE_URL;
 		RequestSpecification request = RestAssured.given();
 		request.header("Authorization", "Bearer " + GlobalContants.TOKEN).header("x-tenant-id", "melinda").header("Content-Type", "application/json");
-		response = request.body("{\"name\":\"" + updateVendor+"\"}").delete("/api/integration-svc/vendors");
+		response = request.get("/api/integration-svc/vendors/"+ vendorID);
+		Assert.assertEquals(response.jsonPath().get("data.id"), vendorID);
+		Assert.assertEquals(response.jsonPath().get("data.name"), updateVendor);
+		
+	}
+	
+	@Test
+	public void Test_04_Delete_Vendor() {
+		RestAssured.baseURI = GlobalContants.BASE_URL;
+		RequestSpecification request = RestAssured.given();
+		request.header("Authorization", "Bearer " + GlobalContants.TOKEN).header("x-tenant-id", "melinda").header("Content-Type", "application/json");
+		response = request.body("{\"name\":\"" + updateVendor+"\"}").delete("/api/integration-svc/vendors/"+ vendorID);
 		Assert.assertEquals(response.getStatusCode(), 200);
+		Assert.assertEquals(response.jsonPath().get("data.task"), "deleted");
 	}
 
 }
